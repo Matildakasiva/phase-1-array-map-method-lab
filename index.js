f68b029adac35e0c383114e0e6fc316697cc2032
Matilda <ogarimatilda@gmail.com>
2024-04-06 12:29:46 +0300

    assignment completed

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(sentence => {
+    const words = sentence.split(' ');
+    const capitalizedWords = words.map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    return capitalizedWords.join(' ');
+  });
+};
+
+console.log(titleCased());
